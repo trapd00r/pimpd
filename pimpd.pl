6630bde470a804f18edc79b2da3b12816e30fddb
trapd00r <trapd00r@trapd00r.se>
2010-03-12 13:14:04 +0100

    Added LICENSE and copyright notice

--- pimpd.pl
+++ pimpd.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl 
-###############################################################################
-##         FILE:   pimpd.pl
-#         USAGE:  ./pimpd.pl
-#   DESCRIPTION:  Perl Interface for the Music Player Daemon
-#        AUTHOR:  trapd00r (trapd00r at trapd00r dot se)
-#       VERSION:  1.0.0
-#       CREATED:  2010-01-09 09:29:44 AM
-###############################################################################
+# pimpd - Perl Interface for the Music Player Daemon
+# Copyright (C) 2010 trapd00r <trapd00r@trapd00r.se>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use Audio::MPD;
